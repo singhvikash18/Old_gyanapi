f25636927f623b0b10f4d610f6759c916ad9b861
vikash singh <vik18nov@gmail.com>
2021-11-26 11:06:46 +0530

    email sent

--- controller/mailController.js
+++ controller/mailController.js
@@ -1,2 +1,24 @@
+const mailservicecontroller = require('../service/mail');
+const   mailroutecontroller = require('../routes/index');
+const httpStatus = require("http-status")
+const { response } = require('express');
 
-    
+const controlmail = async (req,res)=>{
+    const user = await mailservicecontroller.servicemail(req.body.html);
+    console.log("sdsds");
+    const response = {user:user};
+    const data ={
+        itemcount : 2,
+        status_code : httpStatus.OK,
+        message: "successfully sent",
+        data: response,
+        
+    };
+    res.status(httpStatus.OK).send(data)
+};
+
+const mailcontroller ={
+    controlmail,
+}
+
+module.exports =mailcontroller;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const getmails = require('../service/mail');
 
-router.post('/test',getmails.servicemail);
+router.post('/email_send',getmails.servicemail);
 
 module.exports =router;
