9106195214e3a45700d568c3e9913b8c9e681514
vikash singh <vik18nov@gmail.com>
2022-01-11 15:08:55 +0530

    save

--- service/subscription_services.js
+++ service/subscription_services.js
@@ -3,6 +3,8 @@ const router = require('express').Router();
 const subs = require('../model/subscription_table');
 const AppError = require('../utils/app_error');
 const httpStatus = require('http-status');
+const { isNull } = require('lodash');
+
 const subsServices = async(req,res)=>{
     const ss = await subs.find();
     return ss;
@@ -11,13 +13,17 @@ const subsServices = async(req,res)=>{
 const subsCategoryServices = async(categoryId)=>{
     console.log(categoryId)
     // const ObjectId = mongoose.Types.ObjectId;
-    try{
+    
+    
     const scs = await subs.find({category_id:categoryId}).populate("category_id")
+    if(scs ==0){
+        throw new AppError(httpStatus.BAD_REQUEST, "Payment not found");
     }
-    catch(err){
-        throw new AppError(httpStatus.BAD_REQUEST, "subs not found");
-    }
+   
+
     return scs;
+
 }
 
-module.exports = {subsServices,subsCategoryServices,}
+
+module.exports = {subsServices,subsCategoryServices,};
