bd69b80c16e8e26fe3ffeb7c2098fc51f9d225da
vikash singh <vik18nov@gmail.com>
2022-01-13 12:57:56 +0530

    save

--- model/payment_model.js
+++ model/payment_model.js
@@ -41,10 +41,6 @@ const paymentSchema = new mongoose.Schema ({
     paymentEndTime :{
         type: Date,
         required:true
-    },
-    subscriptionid:{
-        type:String,
-        required:true
     }
 
     
@@ -56,3 +52,102 @@ const paymentSchema = new mongoose.Schema ({
 );
 
 module.exports = mongoose.model('payment',paymentSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { ObjectId } = require('mongodb')
+// const mongoose =require('mongoose')
+
+// const paymentSchema = new mongoose.Schema ({
+//     amount : {
+//         type : String,
+//         required : true,   
+//     },
+//     payeeEmail : {
+//         type : String,
+//         required : true
+//     },
+//     paymentCreatedTime : {
+//         type : Date,
+//         required : true
+//     },
+//     paymentGateway : {
+//         type : String,
+//         required: true
+//     },
+//     paymentId : {
+//         type : String,
+//         required : true
+//     },
+//     user : {
+//         type : ObjectId,
+//         required: true
+//     },
+//     category_id  : {
+//         type : ObjectId,
+//         required : true
+//     },
+//     duration :{
+//         type: String,
+//         required : true
+//     },
+//     paymentStartTime :{
+//         type: Date,
+//         required:true
+//     },
+//     paymentEndTime :{
+//         type: Date,
+//         required:true
+//     },
+//     subscriptionid:{
+//         type:String,
+//         required:true
+//     }
+
+    
+
+// },
+// {
+//     timestamps:true
+// }
+// );
+
+// module.exports = mongoose.model('payment',paymentSchema);
