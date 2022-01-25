14645a227ea5ae6467b31b145d8ab8099d338f40
vikash singh <vik18nov@gmail.com>
2022-01-25 18:47:46 +0530

    save

--- service/notification_services.js
+++ service/notification_services.js
@@ -4,7 +4,12 @@ const notifytable = require('../model/notification_table');
 
 const notifyserives =async(categoryId)=>{
     const ns = await notifytable.find({category_id:categoryId});
-    return ns;
+    if(ns){
+        return ns;}
+        else{
+            throw new AppError(httpStatus.BAD_REQUEST, "videoDetials not found");  
+    
+        }
 }
 
 const notificatiVideoService = async(notifyid)=>{
