fdc72bdbedecdeacf3e77e96c9e419bcd59c4455
vikash singh <vik18nov@gmail.com>
2022-01-27 15:24:28 +0530

    save

--- service/notification_services.js
+++ service/notification_services.js
@@ -5,13 +5,14 @@ const notifytable = require('../model/notification_table');
 
 
 const notificatiVideoService = async(notifyid)=>{
-    const nvs = await notifytable.findOne({user_id:notifyid}).populate("videoid");
-   if(nvs){
+    const nvs = await notifytable.find({user_id:notifyid}).populate("videoid");
     return nvs;
-   }
-   else{
-    throw new AppError(httpStatus.BAD_REQUEST, "notificationDetials not found");  
-   }
+//    if(nvs){
+//     return nvs;
+//    }
+//    else{
+//     throw new AppError(httpStatus.BAD_REQUEST, "notificationDetials not found");  
+//    }
 }
 
 module.exports = {notificatiVideoService,}
