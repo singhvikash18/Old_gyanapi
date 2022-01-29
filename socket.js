b047013707419094f1dfed8457aad4ba741b3e26
vikash singh <vik18nov@gmail.com>
2022-01-29 17:34:53 +0530

    save

--- socket.js
+++ socket.js
@@ -1,29 +1,47 @@
+const { result } = require("lodash");
 var socketio = require("socket.io");
 var io ;
+const notificationTable = require('./model/notification_table');
 
 
 const initSocketIo = {};
 const sesionRoom = [];
 
 initSocketIo.init = (server) =>{
-  io = socketio(server
-    ,{
+  io = socketio(server,{
     cors: {
       origin: "https://gyanais.vercel.app",
       credentials: true
     }
-  }
-  );
-    io.on("connection",(socketio)=>{
+  });
+  io.on("connection",(socketio)=>{
         console.log('connection is done.');
-        socketio.on("disconnect",(socketio)=>{
-            console.log('connection is closed')
+
+
+        //get userid
+        socketio.on('userid', async(userid)=>{
+          if(userid !==null){
+            
+          
+          console.log(userid);
+         
+          let result = await notificationTable.find({user_id:userid})
+            console.log(result)
+           io.to(socketio.id).emit('notification', result);
+          
+        }
+        
+
         })
+        socketio.on("disconnect",(socketio)=>{
+          console.log('connection is closed');
+      });
+      
 
-    })
-    
-
-
+       
+      })
+       
+   
 }
 
 initSocketIo.getIO = () => {
