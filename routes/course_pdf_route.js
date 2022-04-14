61843bf87e46ac5e958780a391fa44a28a5cb7f3
vikash singh <vik18nov@gmail.com>
2022-04-14 12:35:21 +0530

    sdsds

--- routes/course_pdf_route.js
+++ routes/course_pdf_route.js
@@ -1,6 +1,28 @@
-const router =require('express').Router();
+const router = require('express').Router();
 const pdfcontrol = require('../controller/course_video_pdf_controller');
 
-router.get('/',pdfcontrol.course_pdf_Controller);
+
+const folderpath = './profile/fileuploads'
+const multer = require('multer')
+const fs = require('fs');
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, folderpath)
+    },
+    filename: async function(req, file, cb) {
+
+        cb(null, file.originalname);
+
+        
+    }
+
+});
+var upload = multer({ storage: storage });
+
+
+router.post('/uploadfile', upload.single('pdfName'), pdfcontrol.course_pdf_Controller);
+
 
 module.exports = router;
