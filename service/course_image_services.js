0f6b73fa1c131a005c0f37a566e97c161ff7001c
vikash singh <vik18nov@gmail.com>
2022-04-19 11:22:00 +0530

    postbuild

--- service/course_image_services.js
+++ service/course_image_services.js
@@ -6,7 +6,7 @@ const course_image_services = async(req)=>{
     const getpdfname = await course_pdf_Table.findOne({roomid:req.body.roomid});
 
     const cis = await room_image_table.find({roomid:req.body.roomid, pdfName:getpdfname.pdfName});
-    console.log(cis)
+   
     if(cis){
         return cis;
  
